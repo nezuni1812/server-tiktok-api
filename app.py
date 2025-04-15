24c1dc6b77d735bb6e20d98a6e06e9c70c4cd370
nezuni1812 <132083873+nezuni1812@users.noreply.github.com>
2025-04-16 01:41:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ cors = CORS(app, resources={
 
 port = os.getenv("PORT") or 4000
 CLIENT_KEY = os.getenv("TIKTOK_CLIENT_KEY")
-CLIENT_SECRET = setenv("TIKTOK_CLIENT_SECRET")
+CLIENT_SECRET = os.getenv("TIKTOK_CLIENT_SECRET")
 REDIRECT_URI = "https://server-tiktok-api.onrender.com/callback"
 SCOPES = "user.info.basic,video.list"
 
